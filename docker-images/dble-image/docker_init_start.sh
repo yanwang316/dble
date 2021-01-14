4ab94ef2c24fe77f668e46090d42fa9a3cbe7761
yanwang <yanwang@5i5j.com>
2021-01-14 14:40:49 +0800

    docker打包脚本微调

--- docker-images/dble-image/docker_init_start.sh
+++ docker-images/dble-image/docker_init_start.sh
@@ -26,8 +26,6 @@ cp -n /opt/dble/extend.conf.d/* /opt/dble/conf/
 
 sh /opt/dble/bin/dble start
 sh /opt/dble/bin/wait-for-it.sh 127.0.0.1:8066
-# mysql -P9066 -u man1 -h 127.0.0.1 -p654321 -e "create database @@shardingNode ='dn1,dn2,dn3,dn4,dn5,dn6'"
-# mysql -P8066 -u root -h 127.0.0.1 -p123456 -e "source /opt/dble/conf/template_table.sql" testdb
 
 if [ -x "/opt/dble/conf/dble_init.sh" ]; then
   sh /opt/dble/conf/dble_init.sh
